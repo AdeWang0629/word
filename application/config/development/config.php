cae4953c8109fddcc6362e9e89c8373523038a04
AdeWang0629 <adswang.dev@gmail.com>
2019-05-15 14:43:56 +0600

    uload wordapp.php view and it is working now

--- application/config/development/config.php
+++ application/config/development/config.php
@@ -16,7 +16,7 @@
 */
 
 if($_SERVER['SERVER_NAME'] === 'localhost'){
-    $config['base_url'] = 'http://localhost/keipro_all/word/';
+    $config['base_url'] = 'http://localhost/word/';
 }elseif($_SERVER['SERVER_NAME'] === '192.168.100.120'){
     $config['base_url'] = 'http://192.168.100.120:8080/word/';
 }elseif ($_SERVER['SERVER_NAME'] === '120') {
