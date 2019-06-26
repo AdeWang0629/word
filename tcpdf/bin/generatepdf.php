d525b3eb8671d066e7057052a9da22adb082c5a2
AdeWang0629 <adswang.dev@gmail.com>
2019-06-26 15:45:07 +0600

    test

--- tcpdf/bin/generatepdf.php
+++ tcpdf/bin/generatepdf.php
@@ -84,7 +84,6 @@ else {
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-
 </head>
 
 <body>
