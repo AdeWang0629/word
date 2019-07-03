e8e47d8ed2bf4f441310f3b2a315d2736b7614d1
AdeWang0629 <adswang.dev@gmail.com>
2019-07-03 16:07:02 +0600

    Uploaded

--- tcpdf/bin/generatepdf.php
+++ tcpdf/bin/generatepdf.php
@@ -80,10 +80,11 @@ else {
     ?>
 <!DOCTYPE html>
 <html lang="jp" >
-<head>';
+<head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<style>.page_separator{text-align:right;display:none;}</style>
 </head>
 
 <body>
